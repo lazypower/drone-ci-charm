ab43a4df46f89b5c76d3d50bf0a9e25278fbbcb3
Charles Butler <chuck@dasroot.net>
2015-07-13 01:57:30 -0400

    Move the workaround to config-changed with proper context

--- hooks/hooks.py
+++ hooks/hooks.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os
 import sys
 from path import path
 from charmhelpers.contrib.ansible import AnsibleHooks
@@ -7,7 +8,6 @@ from charmhelpers.contrib.ansible import AnsibleHooks
 # Create the hooks helper, passing a list of hooks which will be
 # handled by default by running all sections of the playbook
 # tagged with the hook name.
-
 
 def hook_names(here=path(__file__).parent):
     for name in (x.basename()
@@ -17,6 +17,14 @@ def hook_names(here=path(__file__).parent):
 
 hooks = AnsibleHooks(playbook_path='playbooks/site.yaml',
                      default_hooks=list(hook_names()))
+
+if not os.path.exists('/etc/ansible'):
+    os.mkdir('/etc/ansible')
+    # Work around some wonkyness with ansible
+    with open('/etc/ansible/hosts', 'w') as f:
+        f.write('localhost')
+
+
 
 if __name__ == "__main__":
     hooks.execute(sys.argv)
